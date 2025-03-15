e4bfe46d736ea6e484a93f478a204195a07065f4
CalmFarm <vhzpxj@gmail.com>
2025-03-16 00:26:41 +0900

    Initial commit

---
+++ Assets/Script/Weapon.cs
@@ -0,0 +1,44 @@
+using StarterAssets;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] GameObject hitVFXPrefab;
+    [SerializeField] Animator animator;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] int damage = 1;
+
+    StarterAssetsInputs starterAssetsInputs;
+
+    const string SHOOT_STRING = "Shoot";
+
+    void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
+
+    void Update()
+    {
+        HandleShoot();
+    }
+
+    void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot) return;
+
+        muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100f))
+        {
+            Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);
+
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth?.TakeDamage(damage);
+        }
+
+    }
+}
